ad88db7c208fa36b4371393c7a173f3d55c4d8ab
AlcidesTiago <alcidesft6@gmail.com>
2020-01-04 07:01:13 +0530

    first commit

---
+++ CustomLogin/View Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  CustomLogin
+//
+//  Created by Alcides Tiago on 04/01/20.
+//  Copyright © 2020 iAFT. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CustomLogin/View Controllers/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  CustomLogin
+//
+//  Created by Alcides Tiago on 03/01/20.
+//  Copyright © 2020 iAFT. All rights reserved.
+//
+
+import UIKit
+import AVKit
+
+var videoPlayer:AVPlayer?
+var videoPlayerLayer:AVPlayerLayer?
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signUpButton: UIButton!
+     
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loadin g the view.
+        
+        setUpElements()
+    }
+        override func viewWillAppear(_ Animated: Bool) {
+            
+        // setUp Video background
+            setUpVideo()
+    }
+    
+    func setUpElements() {
+        
+        Utilities.styleFilledButton(signUpButton)
+        
+        Utilities.styleFilledButton(loginButton)
+        
+    }
+    func setUpVideo(){
+         //Get the path to the resource in the bundle
+       let bundlePath = Bundle.main.path(forResource: "loginbg", ofType: "mp4")
+        
+        guard bundlePath != nil else {
+            return
+        }
+        //Create a URL from it
+        let url = URL(fileURLWithPath: bundlePath!)
+        
+        //Create video player item
+        let item = AVPlayerItem(url: url)
+        //Create the player
+        videoPlayer =  AVPlayer(playerItem: item)
+        //Create layer
+        videoPlayerLayer = AVPlayerLayer(player: videoPlayer!)
+        //Adjust the size and frame
+        videoPlayerLayer?.frame = CGRect(x:
+            -self.view.frame.size.width*1.5, y:0, width:
+            self.view.frame.size.width*4, height:
+            self.view.frame.size.height)
+        
+        view.layer.insertSublayer(videoPlayerLayer!, at: 0)
+        //Add it to the view and play it
+        videoPlayer?.playImmediately(atRate: 0.3)
+        
+        
+        
+    }
+
+}
+
